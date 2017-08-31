28a85d56d615fc6b803b48b3b6a9d6fcca9d3fc8
Fabian <accounts@fabianbalsiger.ch>
2017-08-31 11:03:48 +0200

    docs(classifier): fix titleunderline too short

--- mialab/classifier/__init__.py
+++ mialab/classifier/__init__.py
@@ -6,7 +6,7 @@ Classifier (:mod:`classifier` package)
 This package provides supervised machine learning classifiers.
 
 The decision forest module (:mod:`classifier.decision_forest`)
---------------------------------------------------
+--------------------------------------------------------------
 
 .. autoclass:: classifier.decision_forest.DecisionForestParameters
 
