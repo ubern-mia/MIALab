fe4265b70e0584a1272edc13253c02c212bab9e1
Fabian <accounts@fabianbalsiger.ch>
2017-09-19 20:38:14 +0200

    fix(conf): Sphinx warnings

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,8 @@ import sys
 
 
 sys.path.insert(0, os.path.abspath('.'))  # docs directory
-sys.path.insert(0, os.path.abspath('../mialab'))  # root directory of project
+sys.path.insert(0, os.path.abspath('..'))  # root directory of project
+sys.path.insert(0, os.path.abspath('../mialab'))  # root directory of mialab package
 
 # -- General configuration ------------------------------------------------
 
@@ -104,7 +105,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
