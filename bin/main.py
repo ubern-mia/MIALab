e67655597053a58f1a9e575f2e153aa8aba75654
Fabian <accounts@fabianbalsiger.ch>
2018-07-18 17:33:54 +0200

    fix(main): label shape warning in forest.fit

--- bin/main.py
+++ bin/main.py
@@ -60,13 +60,11 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
 
     # generate feature matrix and label vector
     data_train = np.concatenate([img.feature_matrix[0] for img in images])
-    labels_train = np.concatenate([img.feature_matrix[1] for img in images])
+    labels_train = np.concatenate([img.feature_matrix[1] for img in images]).squeeze()
 
     forest = sk_ensemble.RandomForestClassifier(max_features=images[0].feature_matrix[0].shape[1],
                                                 n_estimators=20,
                                                 max_depth=25)
-
-    print(forest)
 
     start_time = timeit.default_timer()
     forest.fit(data_train, labels_train)
