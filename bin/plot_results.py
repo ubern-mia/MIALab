7ec7919993259c6157ef93787922341514e4a44b
alain <alain.benninger@hotmail.com>
2019-09-23 08:43:52 +0200

    feat(exercise): add boxplot exercise

---
+++ bin/plot_results.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    # todo: load the "results.csv" file from the mia-results directory
+    # todo: read the data into a list
+    # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus) in a boxplot
+
+    # alternative: instead of manually loading/reading the csv file you could also use the pandas package
+    # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
+    pass  # pass is just a placeholder if there is no other code
+
+
+if __name__ == '__main__':
+    main()
