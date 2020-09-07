bea0ce33996ee715d9bf667d28ca7993e40fa010
fabianbalsiger <accounts@fabianbalsiger.ch>
2020-09-07 17:02:25 +0200

    fix(hello_world): update to pymia==0.3.1

--- bin/hello_world.py
+++ bin/hello_world.py
@@ -40,7 +40,7 @@ def main():
     # d = crf.DenseCRF(1000, 2)
 
     # --- pymia
-    eval = pymia_eval.Evaluator()
+    eval = pymia_eval.SegmentationEvaluator([], {})
 
     print('Everything seems to work fine!')
 
