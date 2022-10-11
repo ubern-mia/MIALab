cccb7fafcafc23e30e471f5270de1d447e04f5b7
Julian <julian.lehmann@bluewin.ch>
2022-10-11 15:09:28 +0200

    added boxplot

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def main():
@@ -10,8 +11,9 @@ def main():
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
     # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
-    pass  # pass is just a placeholder if there is no other code
-
+    #pass  # pass is just a placeholder if there is no other code
+    results = pd.read_csv('2022-10-11-09-14-44/results.csv', sep=';')
+    results.boxplot(by='LABEL', column=['DICE'], grid=False)
 
 if __name__ == '__main__':
     main()
