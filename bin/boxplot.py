2aae38e4c9ee539ab86d77a29c4a71b42d690acd
Newton001 <Newtonkelvin75@outlook.com>
2023-10-28 13:53:22 +0200

    Preprocessing of data is completed

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -5,14 +5,17 @@ import csv
 
 
 def main():
-    # todo: load the "results.csv" file from the mia-results directory
+    #
+    # load the "results.csv" file from the mia-results directory
     df = pd.read_csv(r"results.csv", delimiter=';')
-    # todo: read the data into a list
+    #
+    # read the data into a list
     # Extract the Dice coefficients for each label
     labels = ['white matter', 'gray matter', 'hippocampus', 'amygdala', 'thalamus']
     filtered_df = df[df['LABEL'].isin(labels)]
 
-    # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
+    #
+    # plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
     # Create a boxplot for each label
     plt.figure(figsize=(10, 6))
     plt.boxplot([filtered_df[filtered_df['LABEL'] == label]['DICE'] for label in labels], labels=labels)
