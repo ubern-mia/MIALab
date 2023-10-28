002b818e286c70e61fd5555cf0692c086daf92f0
Newton001 <Newtonkelvin75@outlook.com>
2023-10-28 03:11:03 +0200

    Box plot TODO Implemented

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def main():
     # todo: load the "results.csv" file from the mia-results directory
+    df = pd.read_csv(r"C:\Users\newto\OneDrive\BioMedicalEngineering\Notes and Class Documents\FS2024\05_Medical Image Analysis Lab\final_project\MIALab_Lukas_Studer\bin\mia-result")
     # todo: read the data into a list
+    # Extract the Dice coefficients for each label
+    labels = ['white matter', 'gray matter', 'hippocampus', 'amygdala', 'thalamus']
+
     # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
+    # Create a boxplot for each label
+    plt.figure(figsize=(10, 6))
+    plt.boxplot([df[label] for label in labels], labels=labels)
+    plt.title('Dice Coefficients per Label')
+    plt.ylabel('Dice Coefficient')
+    plt.xlabel('Label')
+    plt.grid(axis='y')
+    plt.show()
     #  in a boxplot
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
