cc060b511c379ae5a3654757974660ebcd8a0dc6
Newton001 <Newtonkelvin75@outlook.com>
2023-10-28 09:50:21 +0200

    Implemented BoxPlot, Remaining to check if the values ate plotted well, to the expectations of the prof

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import csv
 
 
 def main():
     # todo: load the "results.csv" file from the mia-results directory
-    df = pd.read_csv(r"C:\Users\newto\OneDrive\BioMedicalEngineering\Notes and Class Documents\FS2024\05_Medical Image Analysis Lab\final_project\MIALab_Lukas_Studer\bin\mia-result")
+    df = pd.read_csv(r"results.csv", delimiter=';')
     # todo: read the data into a list
     # Extract the Dice coefficients for each label
     labels = ['white matter', 'gray matter', 'hippocampus', 'amygdala', 'thalamus']
+    filtered_df = df[df['LABEL'].isin(labels)]
 
     # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
     # Create a boxplot for each label
     plt.figure(figsize=(10, 6))
-    plt.boxplot([df[label] for label in labels], labels=labels)
+    plt.boxplot([filtered_df[filtered_df['LABEL'] == label]['DICE'] for label in labels], labels=labels)
     plt.title('Dice Coefficients per Label')
     plt.ylabel('Dice Coefficient')
     plt.xlabel('Label')
